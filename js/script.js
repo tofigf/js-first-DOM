663d8725a9b5bb769683c7834b0a5a9640309ce0
Tofiq Faracullayev <tofigshf@code.edu.az>
2018-01-19 16:43:09 +0400

    dom commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function mOver(obj) {
+    
+    obj.style.backgroundColor="red"
+    obj.style.transition = "0.7"; 
+}
+function mDown(obj) {
+    obj.style.backgroundColor = "blue";
+}
+function mUp(obj) {
+    obj.style.backgroundColor="yellow";
+    obj.style.transition = "0.7s"; 
+}
